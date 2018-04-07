bb552b66784385b3904990cdc3a39cfc7eeeee47
Alex Mendes da Costa <alexmdac@gmail.com>
2018-04-07 00:16:57 -0700

    chess http server that uses stockfish

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from chess import Board
+from chess.uci import popen_engine
+from flask import Flask, jsonify, request
+
+MOVE_MS = 50
+
+engine = popen_engine('stockfish')
+engine.uci()
+
+app = Flask(__name__)
+
+@app.route('/make_move', methods=['POST'])
+def make_move():
+    req = request.get_json(force=True)
+    try:
+        board = Board(req['fen'])
+    except (ValueError, KeyError):
+        return '', 400
+    engine.ucinewgame()
+    engine.position(board)
+    best_move = engine.go(movetime=MOVE_MS)[0]
+    board.push(best_move)
+    return jsonify({
+        'best_move': best_move.uci(),
+        'new_fen': board.fen(),
+    })
